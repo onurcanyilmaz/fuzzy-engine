c914268557668bd717364536f68a867f3a53fa69
Onurcan Yılmaz <onurcan.yilmaz@sensormatic.com.tr>
2021-07-29 03:02:44 +0300

    Log severity Fixes #11

--- ElasticLoggingService/Startup.cs
+++ ElasticLoggingService/Startup.cs
@@ -1,6 +1,6 @@
-using System;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 
 namespace ElasticLoggingService
 {
@@ -25,78 +25,61 @@ namespace ElasticLoggingService
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ElasticLoggingService", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ElasticLoggingService v1"));
-            }
-
             app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-
 
             var logger = app.ApplicationServices.GetRequiredService<ILogger<Startup>>();
 
-            app.Map("/map1", app =>
+            app.Map("/Log", app =>
             {
                 app.Run(async context =>
                 {
-                    var branchVer = context.Request.Query["branch"];
+                    if (context.Request.Method != "POST")
+                    {
+                        await WriteAsync(context, "the hethod http verb must be a POST!");
+                        return;
+                    }
 
-                    logger.LogInformation("map1 ~ branch info = " + branchVer);
-                    await context.Response.WriteAsync($"Branch used = {branchVer}");
+                    var body = context.Request.Body;
+                    LogRequest logObj = await GetLogRequestObjectAsync(body);
+
+                    if (logObj == null)
+                    {
+                        await WriteAsync(context, "the request body which is not valid");
+                        return;
+                    }
+
+                    if (logObj.Secret != Configuration["Secret"])
+                    {
+                        await WriteAsync(context, "the secret key which is not valid");
+                        return;
+                    }
+
+                    WriteLog(logObj);
+                    await WriteAsync(context, "succes!");
                 });
-            });
 
-            app.Map("/map2", app =>
-            {
-                app.Run(async context =>
+                async Task WriteAsync(HttpContext context, string message)
                 {
-                    string body = string.Empty;
-                    var request = context.Request;
-                    using (var reader
-                                      = new StreamReader(request.Body, Encoding.UTF8, true, 1024, true))
-                    {
-                        body = await reader.ReadToEndAsync();
-                    }
-                    logger.LogInformation(body);
-                    Console.WriteLine(body);
-                });
-            });
+                    await context.Response.WriteAsync(message);
+                }
 
-            app.Map("/map3", app =>
-            {
-                app.Run(async context =>
+                async Task<LogRequest> GetLogRequestObjectAsync(Stream body)
                 {
-                    try
+                    using (var reader = new StreamReader(body, Encoding.UTF8, true, 1024, true))
                     {
-                        throw new Exception("MyCustomError");
+                        return JsonConvert.DeserializeObject<LogRequest>(await reader.ReadToEndAsync());
                     }
-                    catch (Exception ex)
-                    {
-                        logger.LogError(ex, "Unknown error");
-                    }
-                    await context.Response.WriteAsync("Bir hata oluştu");
-                });
+                }
+
+                void WriteLog(LogRequest request)
+                {
+                    logger.Log(request.LogLevel, request.Exception, request.Message);
+                }
             });
         }
     }
